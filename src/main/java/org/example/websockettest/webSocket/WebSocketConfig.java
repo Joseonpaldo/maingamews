1aefc10999f764adc704c3a048ae8917198d36d2
dongwoobae <https://github.com/dongwoobae/naver0314devops11.git>
2024-09-06 13:57:35 +0900

    friend init commit

--- src/main/java/org/example/websockettest/webSocket/WebSocketConfig.java
+++ src/main/java/org/example/websockettest/webSocket/WebSocketConfig.java
@@ -19,7 +19,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
-                .setAllowedOrigins("http://localhost:3000") // CORS 설정 추가
+                .setAllowedOrigins("*") // CORS 설정 추가
                 .withSockJS();
     }
 }
