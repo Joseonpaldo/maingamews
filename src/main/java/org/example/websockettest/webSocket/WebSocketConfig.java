11987cee4187dcb8d827d97cb389f1a16e30beb9
minseok0507 <163380572+minseok0507@users.noreply.github.com>
2024-09-09 17:41:39 +0900

    Update WebSocketConfig.java

--- src/main/java/org/example/websockettest/webSocket/WebSocketConfig.java
+++ src/main/java/org/example/websockettest/webSocket/WebSocketConfig.java
@@ -19,7 +19,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
-                .setAllowedOrigins("http://localhost:3000")  // CORS 설정 추가
+                .setAllowedOrigins("https://joseonpaldo.site/")  // CORS 설정 추가
                 .withSockJS();
     }
 }
